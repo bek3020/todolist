fc08a721f5fc66d475ed09b772c6c719376c398e
Behruz <ismoilovmashhurbek17@gmail.com>
2025-11-10 18:08:40 +0500

    ozgarish

--- src/cars.js
+++ src/cars.js
@@ -1,9 +1,17 @@
+
+import card1 from "./assets/img/card1.png";
+import card2 from "./assets/img/card2.png";
+import card3 from "./assets/img/card3.png";
+import card4 from "./assets/img/card4.png";
+import card5 from "./assets/img/card5.png";
+import card6 from "./assets/img/card6.png";
+
 export const cars = [
   {
     id: 1,
     name: "Ford Fiesta",
     type: "Economy Car",
-    image: "../src/assets/img/card1.png",
+    image: card1,
     seats: 5,
     transmission: "Automatic",
     age: "21+ Years",
@@ -14,7 +22,7 @@ export const cars = [
     id: 2,
     name: "Nissan Versa",
     type: "Compact Car",
-    image: "../src/assets/img/card2.png",
+    image: card2,
     seats: 5,
     transmission: "Automatic",
     age: "21+ Years",
@@ -25,7 +33,7 @@ export const cars = [
     id: 3,
     name: "Toyota Corolla",
     type: "Mid-size Car",
-    image: "../src/assets/img/card3.png",
+    image: card3,
     seats: 5,
     transmission: "Automatic",
     age: "21+ Years",
@@ -36,7 +44,7 @@ export const cars = [
     id: 4,
     name: "Nissan Rogue",
     type: "Mid-size SUV",
-    image: "../src/assets/img/card4.png",
+    image: card4,
     seats: 5,
     transmission: "Automatic",
     age: "21+ Years",
@@ -47,7 +55,7 @@ export const cars = [
     id: 5,
     name: "Chevy Traverse",
     type: "Full-size SUV",
-    image: "../src/assets/img/card5.png",
+    image: card5,
     seats: 5,
     transmission: "Automatic",
     age: "21+ Years",
@@ -58,7 +66,7 @@ export const cars = [
     id: 6,
     name: "Nissan Altima",
     type: "Full-size Car",
-    image: "../src/assets/img/card6.png",
+    image: card6,
     seats: 5,
     transmission: "Automatic",
     age: "21+ Years",
